1218c9152821676284ac1440426aab53b016eb39
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-02-25 10:46:37 +0300

    init

---
+++ build/createPage.js
@@ -0,0 +1,22 @@
+const { mkdirSync, writeFileSync } = require('fs');
+const { getDirectoriesBasenames } = require('./utils.js');
+const dirs = require('./dirs');
+
+const [execPath, selfPath, pageName] = process.argv;
+const existingPages = getDirectoriesBasenames(`${dirs.pages}`);
+
+if (existingPages.includes(pageName)) {
+    throw new Error(`Page with the name ${pageName} already exists`);
+}
+
+const pagePath = `${dirs.pages}/${pageName}`;
+
+//Create Directory
+mkdirSync(pagePath);
+
+//styles
+writeFileSync(`${pagePath}/${pageName}.scss`, '');
+//markup
+writeFileSync(`${pagePath}/${pageName}.pug`, '');
+//js
+writeFileSync(`${pagePath}/${pageName}.js`, '');
