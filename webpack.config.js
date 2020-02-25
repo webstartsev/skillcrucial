f6813c70ceb8f9ca1f20bc0403e0cc72bf69513b
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-02-25 17:09:39 +0300

    update uglify to terser

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const cssnano = require('cssnano');
 
@@ -70,7 +70,7 @@ const config = {
   // },
   optimization: {
     minimizer: [
-      new UglifyJsPlugin(),
+      new TerserPlugin(),
       new OptimizeCSSAssetsPlugin({
         cssProcessor: cssnano,
         canPrint: false
