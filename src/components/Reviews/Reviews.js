6420f0e986c5c3069dee2492c2bedeb9af2d851c
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-02-27 16:20:19 +0300

    update Swiper config

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -2,16 +2,21 @@ import Swiper from 'swiper';
 
 const reviewThumbs = new Swiper('.Reviews-thumbs', {
   slidesPerView: document.querySelectorAll(`.ReviewThumb`).length,
-  simulateTouch: false,
+  touchRatio: 0,
   freeMode: true,
   watchSlidesVisibility: true,
   watchSlidesProgress: true
 });
 
 new Swiper(`.Reviews-wrap`, {
+  simulateTouch: false,
   speed: 100,
   loop: true,
   effect: 'fade',
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false
+  },
   thumbs: {
     swiper: reviewThumbs
   }
