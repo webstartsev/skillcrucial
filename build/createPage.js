62d665234f6a0a2e803843cacafa8ec403fab154
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-02-27 14:09:24 +0300

    update build script

--- build/createPage.js
+++ build/createPage.js
@@ -6,7 +6,7 @@ const [execPath, selfPath, pageName] = process.argv;
 const existingPages = getDirectoriesBasenames(`${dirs.pages}`);
 
 if (existingPages.includes(pageName)) {
-    throw new Error(`Page with the name ${pageName} already exists`);
+  throw new Error(`Page with the name ${pageName} already exists`);
 }
 
 const pagePath = `${dirs.pages}/${pageName}`;
@@ -15,8 +15,14 @@ const pagePath = `${dirs.pages}/${pageName}`;
 mkdirSync(pagePath);
 
 //styles
-writeFileSync(`${pagePath}/${pageName}.scss`, '');
+writeFileSync(`${pagePath}/${pageName}.scss`, `@import '~@/styles/vars';`);
 //markup
-writeFileSync(`${pagePath}/${pageName}.pug`, '');
+writeFileSync(
+  `${pagePath}/${pageName}.pug`,
+  `extends ../layout.pug 
+
+block content
+`
+);
 //js
-writeFileSync(`${pagePath}/${pageName}.js`, '');
+writeFileSync(`${pagePath}/${pageName}.js`, `import './${pageName}.scss';`);
