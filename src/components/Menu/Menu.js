57bfe4725d85b76299767f627de648501a2f034e
Sergey Startsev <sergey.starcev@imeretinka.ru>
2020-02-28 12:24:57 +0300

    add adaptive :)

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,1 +1,7 @@
- 
+const toogleMenu = document.querySelector(`.MenuToggle`);
+const menu = document.querySelector(`.Menu`);
+
+toogleMenu.addEventListener(`click`, function() {
+  menu.classList.toggle(`Menu_show`);
+  toogleMenu.classList.toggle(`MenuToggle_active`);
+});
